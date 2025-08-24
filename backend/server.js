b96a9fe4ae6607243daf18a45e9d4547c072c833
Akash gupta <www.akashgupta45@gmail.com>
2025-08-24 16:43:48 +0530

    cor 4.0

--- backend/server.js
+++ backend/server.js
@@ -10,27 +10,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://fin-track-gray.vercel.app"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow Postman / server calls
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
-
-// ✅ explicitly handle preflight requests
-app.options("*", cors());
+app.use(cors({origin:'https://fin-track-gray.vercel.app/'}))
 
 app.use(bodyParser.json());
 
