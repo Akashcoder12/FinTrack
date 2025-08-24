d5f888b208df96b06f888f30a97ae8526a0edd2b
Akash gupta <www.akashgupta45@gmail.com>
2025-08-24 15:51:57 +0530

    server updated 2.0

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,7 @@ app.use(bodyParser.json());
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
