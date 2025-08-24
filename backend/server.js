b0e23b02d4e4e2afb70abd387d2c890bdd976c75
Akash gupta <www.akashgupta45@gmail.com>
2025-08-24 15:49:17 +0530

    server is updated

--- backend/server.js
+++ backend/server.js
@@ -45,9 +45,9 @@ const connectDB = async () => {
 };
 
 connectDB();
-app.use("/",()=>{
-     res.json({message:"Api is running"});
-})
+// app.use("/",(req,res)=>{
+//      res.json({message:"Api is running"});
+// })
 app.use('/auth', authRoutes);
 app.use('/budget', budgetRoutes);
 
